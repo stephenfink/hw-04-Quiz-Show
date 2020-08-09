b8a001de43b3bf26f647f27f5f8fb604fb4b60e9
Stephen Fink <stephenfink8@gmail.com>
2020-08-09 17:17:06 -0500

    added styling and css for when answers are picked

--- Assets/game.js
+++ Assets/game.js
@@ -3,7 +3,7 @@ var questions
 var score = 0;
 //when i click start it will start the quiz and put the timer on a count down
 var start = document.getElementById("#start")
-start.addEventListener("click", gameShow);//game is a current place holder for event to happen after click to start game
+start.addEventListener("click", gameStart);//game is a current place holder for event to happen after click to start game
 
 // when game starts question one will show on screen where the "This is where questions will be placed"
 // Answer one, two, three, and four will change text in which user may pick one
@@ -15,7 +15,7 @@ start.addEventListener("click", gameShow);//game is a current place holder for e
 //when timer ends the game will stop, until the start button is click to restart it
 //have a high score list base on local user input, also prompt for name after click event to record a high score
 //this will reflect on the high score col. 
-function gameShow();{
+function gameStart();{
     var userName =prompt("Please enter a name.")
     if(userName ==null){
         alert("You must input a username")
@@ -29,7 +29,23 @@ function gameShow();{
     //this is using the user input a name to save it for later
 
     // questions needs to be an in array so it can go in a loop till the timer runs out, the goal is to at least have 10 questions 
+ 
+function questions(){
+
+}
+ 
+
+function answers{
+    
+}
+ 
     // the timer and questions need to pace well with each other, 
+
+
+
+
+
+
 }
 
 // we will use the localStorage to fill in high score
