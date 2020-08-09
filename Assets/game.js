0c61f8d8f252f8f00327bd7495f05b6e78f4bf22
Stephen Fink <stephenfink8@gmail.com>
2020-08-09 14:14:15 -0500

    added high score layout and plan on JS

---
+++ Assets/game.js
@@ -0,0 +1,16 @@
+//This is the code layout for the game show and what we want.
+
+//when i click start it will start the quiz and put the timer on a count down
+var start = document.querySelector("#start")
+start.addEventListener("click", game);//game is a current place holder for event to happen after click to start game
+
+// when game starts question one will show on screen where the "This is where questions will be placed"
+// Answer one, two, three, and four will change text in which user may pick one
+// the timer will count down during the time of user picking the question
+
+// user gets answer correct, it will add one point to score and if user gets question wrong user loses more time
+// bonus have a puase between each question to show user which answer was correct, the time will be puase for five seconds "5000"?
+
+//when timer ends the game will stop, until the start button is click to restart it
+//have a high score list base on local user input, also prompt for name after click event to record a high score
+//this will reflect on the high score col. 
