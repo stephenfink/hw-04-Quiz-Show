2a89df4378260e350f6e3835baf9dbbd1e1c2c7d
Stephen Fink <stephenfink8@gmail.com>
2020-08-09 17:53:49 -0500

    got prompt to work with start button, and console log user info and start button

--- Assets/game.js
+++ Assets/game.js
@@ -1,9 +1,13 @@
 //This is the code layout for the game show and what we want.
 var questions 
 var score = 0;
+var userName
 //when i click start it will start the quiz and put the timer on a count down
-var start = document.getElementById("#start")
-start.addEventListener("click", gameStart);//game is a current place holder for event to happen after click to start game
+
+
+var startBT = document.getElementById("start")
+startBT.addEventListener("click", gameStart)
+//game is a current place holder for event to happen after click to start game
 
 // when game starts question one will show on screen where the "This is where questions will be placed"
 // Answer one, two, three, and four will change text in which user may pick one
@@ -15,24 +19,23 @@ start.addEventListener("click", gameStart);//game is a current place holder for 
 //when timer ends the game will stop, until the start button is click to restart it
 //have a high score list base on local user input, also prompt for name after click event to record a high score
 //this will reflect on the high score col. 
-function gameStart();{
-    console.log(start)
+function gameStart(){
     var userName = prompt("Please enter a name.")
+        localStorage.getItem(userName)
+        console.log(startBT)
+        console.log(userName)
+}
     
-    
+
     
     //this is using the user input a name to save it for later
 
     // questions needs to be an in array so it can go in a loop till the timer runs out, the goal is to at least have 10 questions 
  
-function questions(){
-
-}
+    //function questions(){}
  
 
-function userAnswers(){
-
-}
+    //function userAnswers(){}
  
     // the timer and questions need to pace well with each other, 
 
@@ -41,6 +44,6 @@ function userAnswers(){
 
 
 
-}
+
 
 // we will use the localStorage to fill in high score
