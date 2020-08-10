65ec72b4a228bad416425b220199a920bed51134
Stephen Fink <stephenfink8@gmail.com>
2020-08-10 16:32:32 -0500

    forgot to save main js file

--- Assets/game.js
+++ Assets/game.js
@@ -1,50 +1,5 @@
 //This is the code layout for the game show and what we want.
-var questions = [{
-                "question": "Javascript is what kind of language?",
-                "choice1": "a programming language",
-                "choice2": "a foreign language",
-                "choice3": "a magical language",
-                "choice4": "coffee paper language",
-                "answer": "1",
-}, { 
-                "question": "These buttons has a what type of event listener?",
-                "choice1": "keypress",
-                "choice2": "hearing", 
-                "choice3": "event", 
-                "choice4": "click",
-                "answer": "4",
 
-},{
-                "question": "Now here's a hard one, what is the difference of these operators; && and ||?",
-                "choice1": "AND and NOT",
-                "choice2": "AND and OR", 
-                "choice3": "AND and AND", 
-                "choice4": "OR and AND",
-                "answer": "2",
-}, {
-
-                "question": "Here's an easy one, what is one way we check for errors and inputs on a browser?",
-                "choice1": "check.errer()",
-                "choice2": "error.log()", 
-                "choice3": "input.errorcheck()", 
-                "choice4": "console.log()",
-                "answer": "4",
-},{
-                "question": "Which one is a string?",
-                "choice1": "function hello(world){}",
-                "choice2": "<h1>Hello</h1>", 
-                "choice3": "var greetings= 'hello'", 
-                "choice4": "var hi",
-                "answer": "3",
-},{
-                "question": "what is one way javascript can get storage data?",
-                "choice1": "theCloud.getItem()",
-                "choice2": "getItem.localStorage()", 
-                "choice3": "localStroage.getItem()", 
-                "choice4": "localstorage.getitem()",
-                "answer": "3"
-}
-]
 var score = 0;
 var userName
 var currentQuestion = 0;
