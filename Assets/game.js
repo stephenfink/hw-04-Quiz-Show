fe46cd85510f7324f9d2614a05e02ea6899f0ba4
Stephen Fink <stephenfink8@gmail.com>
2020-08-13 16:11:52 -0500

    going back to take one step at a time to console log each event as I build on to it

--- Assets/game.js
+++ Assets/game.js
@@ -6,9 +6,6 @@ var currentQuestion = 0;
 var questionEl = document.getElementById("question")
 var nextQ = document.getElementById("next")
 //this is where the user and high score mix
-var user = localStorage.setItem("userName") 
-var highScore = localStorage.setItem("score")
-
 var startTime = 70;
 var time = startTime
 var countDownEl = document.getElementById("timer")
@@ -38,120 +35,11 @@ startBT.addEventListener("click", gameStart)
 
 function gameStart(){
     var userName = prompt("Please enter a name.")
-        localStorage.setItem(userName)
+        
         console.log(startBT)
         console.log(userName)
         
     //for(time = 0) {
        // return score 
         
-    }
-
-    function questionL(questionsIndex){
-       var quest = questions[questionsIndex];
-        questionEl.textContent=(questionsIndex) + '. ' + quest.question
-        text1.textContent = quest.choice1;
-        text2.textContent = quest.choice2;
-        text3.textContent = quest.choice3;
-        text4.textContent = quest.choice4;
-    };
-
-    function nextQuest(){
-        var selectedChoice = document.querySelector("input[type=button]:clicked");
-        if(!selectedChoice){
-            alert("Must pick a possible answer");
-            return;
-        }
-        var answer = selectedChoice.value;
-        if(question[currentQuestion].answer == answer){
-            score += 1
-            currentScore = document.getElementById("#scoreNum") 
-            score = currentScore
-            
-        }else{
-            
-        }
-    }
-    setInterval(countingDown, 1200)
-    function countingDown(){
-        var seconds = Math.floor(time);
-        countDownEl.innerHTML = seconds --;
-        countDownEl
-    }
-    localStorage.setItem(score)
-
-// after timer runs to 0 these will be added to the high score
-
-
-//when timer ends the game will stop, until the start button is click to restart it
-//have a high score list base on local user input, also prompt for name after click event to record a high score
-//this will reflect on the high score col. 
-    localStorage.setItem(userName)
-    localStorage.setItem(score)
-    function highScoreChart(){
-        for(var i = 0; i < highScore.score.length; i++){
-            var li = document.createElement("li")
-            li.textContent = userHS.user[i] + ", Score: " + score[i]
-            li.classList.add("listingUsers")
-            list.appendChild(li)
-        }
-    };
-
-    //things to follow suit after this
-    //time number counts down from base time to 0
-    //new question will pop up when user clicks next 
-    //when time reaches 0 the score data and the user name data will be added to high score 
-    //this will stay as local storage
-
-    
-
-    
-    //this is using the user input a name to save it for later
-
-    // questions needs to be an in array so it can go in a loop till the timer runs out, the goal is to at least have 10 questions 
- 
-    //function questions(){}
-        //question  :answer 
-            //question 1 : Javascript is "what" kind of language?
-                //rightanswer 1: a programming language
-                //wronganswers 1: a foreign language, a magical language, coffee paper language
-        
-            //question 2: The button has a what type of event listener?
-                //rightanswer 2 : "click"
-                //wronganswers 2 : "event", "hearing", "keypress"
-
-            //question 3: Now here's a hard one, what is the difference of these operators; "&&"" and "||""
-                //rightanswer 3: "AND" and "OR"
-                //wronganswers: "AND" and "NOT", "AND" and "AND", "OR" and "AND"
-            
-            //question 4: Here's an easy one; How can we check for errors of input using javascript?
-                //rightanswer 4: console.log();
-                //wronganswers: console.errors, error.log, check.error
-
-            //question 5: which one is a string?
-                //rightanswers 5: var greetings = "hello" , const hello ="Greetings"
-                //wronganswers 5: var hi, function hello(){}
-
-            //question 6: what is one way javascript can storage data?
-                //rightanswer 6: localStorage.getItem()
-                //wronganswers 6: localstorage.getItem(), localStorage.getItem{}, The Cloud
-
-            //for each of these we need to have the question fill in where we have the text
-            //for each we need each answer to fill in the text of answer
-            // for each, the right answer must be set with the correct color, and wrong answers with wrong answer color
-            // each time a question is right we add one point to score
-            // each time a question is wrong we take off time
-    //function userAnswers(){}
- 
-    // the timer and questions need to pace well with each other, 
-
-    // need loop for questions till timer runs out
-
-
-
-
-
-
-
-
-// we will use the localStorage to fill in high score
+}
