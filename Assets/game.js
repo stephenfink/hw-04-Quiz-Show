979309dbdd1324fa2b2881e06ea83f6656fe503a
Stephen Fink <stephenfink8@gmail.com>
2020-08-14 15:00:39 -0500

    save point and start of timer

--- Assets/game.js
+++ Assets/game.js
@@ -1,17 +1,27 @@
 var currentQuestion = 0;
 var score = 0;
 var totQuestions = questions.length;
-
+var timeInterval = 0
+var timeLeft = 70
 var questionEl = document.getElementById("question")
 var opt1 = document.getElementById("c1");
 var opt2 = document.getElementById("c2");
 var opt3 = document.getElementById("c3");
 var opt4 = document.getElementById("c4");
 var nextButton = document.getElementById("nextButton");
-
-
-
-
+var time = document.getElementById("time")
+// this sets the time
+function setTime(){
+  timeInterval = setInterval(function(){
+    timeLeft--;
+    time.textContent = timeLeft;
+    if(timeLeft <= 0){
+      timeLeft = 0
+      clearInterval(timeInterval);
+    }
+  },7000);
+}
+setTime()
 
 
 
@@ -26,7 +36,7 @@ function loadQuestion(questionIndex){
     c3.textContent = q.choice3;
     c4.textContent = q.choice4;
 };
-
+//the next question
 function loadNextQuestion(){
     var selectedOption = document.querySelector("input[type=radio]:checked");
     if(!selectedOption){
@@ -48,8 +58,11 @@ function loadNextQuestion(){
       c1.textContent = "Your Score: " + score;
       c2.textContent = "User Name: " +userName;
         return;
+
     }
     loadQuestion(currentQuestion);
 }
 
 loadQuestion(currentQuestion);
+
+//for local storage
