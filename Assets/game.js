2c5d2f6822d89a1ccd618ef50c9e6c143853c7e7
Stephen Fink <stephenfink8@gmail.com>
2020-08-14 19:19:43 -0500

    the first question and timer did not work with start button :

--- Assets/game.js
+++ Assets/game.js
@@ -10,19 +10,19 @@ var opt3 = document.getElementById("c3");
 var opt4 = document.getElementById("c4");
 var start = document.getElementById("startButton")
 var time = document.getElementById("time")
+var nextButton = document.getElementById("nextButton");
 
-
-// this is the question rotation functions
+ //this is the question rotation functions
 function loadQuestion(questionIndex){
   var q = questions[questionIndex];
-  questionEl.textContent = (questionIndex + 1) + ". " + q.question;
+  questionEl.textContent = (questionIndex ++) + ". " + q.question;
   c1.textContent = q.choice1;
   c2.textContent = q.choice2;
   c3.textContent = q.choice3;
   c4.textContent = q.choice4;
 };
 
-start.addEventListener("click", setTime)
+//start.addEventListener("click", setTime)
 
 
 
@@ -37,7 +37,7 @@ function setTime(){
       //quiz should be over
       clearInterval(timeInterval);
     }
-  },7000);
+  },1000);
 }
 setTime()
 
@@ -52,11 +52,12 @@ function loadNextQuestion(){
     if(!selectedOption){
         alert("Please select your answer!");
         return;
+        
     }
     var answer = selectedOption.value;
     if(questions[currentQuestion].answer == answer){
         score += 1;
-        currentQuestion++
+        console.log(answer)
         
     }
     selectedOption.checked = false;
@@ -65,7 +66,7 @@ function loadNextQuestion(){
     if (currentQuestion == totQuestions -1){
         nextButton.textContent = "finish";
     }
-    if (currentQuestion == totQuestions || (timeLeft = 0)){
+    if (currentQuestion == totQuestions){
       var userName= prompt("your name")
       localStorage.setItem(userName)
       localStorage.setItem(score)
@@ -73,10 +74,41 @@ function loadNextQuestion(){
       c1.textContent = "Your Score: " + score;
       c2.textContent = "User Name: " +userName;
         return;
-
+     
     }
+
+    localStorage.setItem(userName , score)
+    
     loadQuestion(currentQuestion);
 }
 
 loadQuestion(currentQuestion);
 
+//GIVEN I am taking a code quiz
+
+//WHEN I click the start button
+// click start buttton
+start.addEventListener("click",)
+console.log(start)
+//THEN a timer starts and I am presented with a question
+  //start button starts timer, and question(0)
+  //timer function
+  //question function
+    // 1 right: 3 wrong
+    //wrong answers is -time
+    //answering right adds to score
+    //answering question moves to next question *6
+    //if user runs out of time 
+  
+
+
+
+
+//WHEN I answer a question
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and score
