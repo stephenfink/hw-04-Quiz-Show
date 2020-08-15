6a3d15d36f3b95f4933dae8831e348102cba8488
Stephen Fink <stephenfink8@gmail.com>
2020-08-14 20:24:29 -0500

    changing from raido/next button into a button format

--- Assets/game.js
+++ Assets/game.js
@@ -4,13 +4,33 @@ var totQuestions = questions.length;
 var timeInterval = 0
 var timeLeft = 70
 var questionEl = document.getElementById("question")
-var opt1 = document.getElementById("c1");
-var opt2 = document.getElementById("c2");
-var opt3 = document.getElementById("c3");
-var opt4 = document.getElementById("c4");
+var btn1 = document.getElementById("c1");
+var btn2 = document.getElementById("c2");
+var btn3 = document.getElementById("c3");
+var btn4 = document.getElementById("c4");
 var start = document.getElementById("startButton");
 var time = document.getElementById("time");
 var nextButton = document.getElementById("nextButton");
+
+
+
+//GIVEN I am taking a code quiz
+
+//WHEN I click the start button
+// click start buttton
+//THEN a timer starts and I am presented with a question
+  //start button starts timer, and question(0)
+  //timer function
+  //question function
+    // 1 right: 3 wrong
+    //wrong answers is -time
+    //answering right adds to score
+    //answering question moves to next question *6
+    //if user runs out of time 
+  
+start.addEventListener("click",)
+
+
 
  //this is the question rotation functions
 function loadQuestion(questionIndex){
@@ -31,7 +51,7 @@ function loadQuestion(questionIndex){
 function setTime(){
   timeInterval = setInterval(function(){
     timeLeft--;
-    time.innerHTML = timeLeft;
+    time.textContent = timeLeft;
     if(timeLeft <= 0){
       timeLeft = 0
       //quiz should be over
@@ -44,62 +64,8 @@ setTime()
 
 
 
-
-
-//the next question
-function loadNextQuestion(){
-    var selectedOption = document.querySelector("input[type=radio]:checked");
-    if(!selectedOption){
-        alert("Please select your answer!");
-        return;
-        
-    }
-    var answer = selectedOption.value;
-    if(questions[currentQuestion].answer == answer){
-        score += 1;
-        console.log(answer)
-        
-    }
-    selectedOption.checked = false;
-    
-    currentQuestion++;
-    if (currentQuestion == totQuestions -1){
-        nextButton.textContent = "finish";
-    }
-    if (currentQuestion == totQuestions){
-      var userName= prompt("your name")
-      localStorage.setItem(userName)
-      localStorage.setItem(score)
-      loc
-      c1.textContent = "Your Score: " + score;
-      c2.textContent = "User Name: " +userName;
-        return;
-     
-    }
-
-    localStorage.setItem(userName , score)
-    
-    loadQuestion(currentQuestion);
-}
-
-loadQuestion(currentQuestion);
-
-//GIVEN I am taking a code quiz
-
-//WHEN I click the start button
-// click start buttton
-start.addEventListener("click",)
 console.log(start)
-//THEN a timer starts and I am presented with a question
-  //start button starts timer, and question(0)
-  //timer function
-  //question function
-    // 1 right: 3 wrong
-    //wrong answers is -time
-    //answering right adds to score
-    //answering question moves to next question *6
-    //if user runs out of time 
-  
+
 
 
 
